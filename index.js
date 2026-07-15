1b9e5032c531f9b6dfa338ee42596b7d44a21901
Adham3870 <mohamedadham6767@gmail.com>
2026-07-15 10:14:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-  const element = React.creatElement ("h1",null,"Welcome to React");
+  const element = React.createElement ("h1",null,"Welcome to React");
         const root = ReactDom.createRoot (document. getElementByid ("root"));
         root.render (element);
